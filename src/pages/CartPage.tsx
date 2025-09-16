1072faab059061b529bd113109b56f521789b160
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-09-16 13:27:47 -0300

    [feat]: cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -7,20 +7,21 @@ const EmptyCart: React.FC = () => {
             <div className="flex flex-col items-center gap-4">
                 <FaArrowLeft className="text-gray-400 text-6xl mb-4" />
 
-                <h2 className="text-xl font-semibold">
-                    Seu carrinho está vazio
-                </h2>
+                <h2 className="text-xl font-semibold">Your cart is empty</h2>
                 <p className="text-gray-500 max-w-md">
-                    Parece que você ainda não adicionou nenhum jogo ao seu
-                    carrinho. Que tal explorar nosso catálogo?
+                    It looks like you haven’t added any games to your cart yet.
+                    How about exploring our catalog?
                 </p>
 
-                <a href="/catalog" className="bg-teal-400 hover:22d3ee text-black font-semibold px-6 py-3 rounded-lg transition">
-                    Explorar Catálogo
+                <a
+                    href="/catalog"
+                    className="bg-teal-400 hover:22d3ee text-black font-semibold px-6 py-3 rounded-lg transition"
+                >
+                    Explore Catalog
                 </a>
 
                 <a href="/" className="text-black hover:underline mt-2">
-                    Voltar ao Início
+                    Back to Home
                 </a>
             </div>
         </div>

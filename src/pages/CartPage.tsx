52589e482a2bcd0f3715b4eec4d3479263486513
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-09-16 13:22:32 -0300

    [feat]: cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,3 +1,30 @@
-export default function CartPage() {
-    return <h1>Meu Carrinho</h1>
+import React from "react"
+import { FaArrowLeft } from "react-icons/fa" // ícone de seta
+
+const EmptyCart: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-center px-4">
+            <div className="flex flex-col items-center gap-4">
+                <FaArrowLeft className="text-gray-400 text-6xl mb-4" />
+
+                <h2 className="text-xl font-semibold">
+                    Seu carrinho está vazio
+                </h2>
+                <p className="text-gray-500 max-w-md">
+                    Parece que você ainda não adicionou nenhum jogo ao seu
+                    carrinho. Que tal explorar nosso catálogo?
+                </p>
+
+                <a href="/catalog" className="bg-teal-400 hover:22d3ee text-black font-semibold px-6 py-3 rounded-lg transition">
+                    Explorar Catálogo
+                </a>
+
+                <a href="/" className="text-black hover:underline mt-2">
+                    Voltar ao Início
+                </a>
+            </div>
+        </div>
+    )
 }
+
+export default EmptyCart

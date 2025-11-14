c03c7bb1047c30c5b02db382c4d9706e3e3ef45f
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-11-14 14:08:02 -0300

    [fix]: deploy issue api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
+import axios from "axios";
 
-export async function fetchData(endpoint: string) {
-  const res = await fetch(`${API_BASE_URL}${endpoint}`);
-  if (!res.ok) throw new Error(`Erro ao buscar ${endpoint}`);
-  return res.json();
-}
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/";
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+});

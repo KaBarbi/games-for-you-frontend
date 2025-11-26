3a1e40d0d6e04bcf4722c58fe2ddd8472cb60281
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-11-26 13:39:00 -0300

    [feat]: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import HomePage from "./pages/HomePage"
-import GameDetailsPage from "./pages/GameDetailsPage"
-import CartPage from "./pages/CartPage"
-import LoginPage from "./pages/LoginPage"
-import RegisterPage from "./pages/RegisterPage"
-import CatalogPage from "./pages/CatalogPage"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import GameDetailsPage from "./pages/GameDetailsPage";
+import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import CatalogPage from "./pages/CatalogPage";
+import Footer from "./components/Footer";
 
 export default function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/games/:id" element={<GameDetailsPage />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/catalog" element={<CatalogPage />} />
-            </Routes>
-        </Router>
-    )
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/games/:id" element={<GameDetailsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="w-full py-6 bg-[#0f0f2f] flex justify-center items-center">
+      <p className="text-gray-300 text-sm">
+        © 2025 <span className="text-teal-400 font-semibold">GamesForYou</span>,
+        All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
